821391cf98e978c790393a1d99b2051f1bebd82d
ae0nic <bzminecraft247@gmail.com>
2024-07-07 16:57:20 -0500

    I got world gen working!!!

--- src/main/java/me/ae0nic/createhighstress/mixin/MoltenVentFeatureMixin.java
+++ src/main/java/me/ae0nic/createhighstress/mixin/MoltenVentFeatureMixin.java
@@ -2,8 +2,10 @@ package me.ae0nic.createhighstress.mixin;
 
 import com.Apothic0n.MoltenVents.api.biome.features.configurations.MoltenVentConfiguration;
 import com.Apothic0n.MoltenVents.api.biome.features.types.MoltenVentFeature;
+import me.ae0nic.createhighstress.init.BlockInit;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,10 +17,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class MoltenVentFeatureMixin {
     @Inject(method = "place", at = @At(value = "HEAD"), cancellable = true)
     private void createHighStress$place(FeaturePlaceContext<MoltenVentConfiguration> pContext, CallbackInfoReturnable<Boolean> cir) {
-        MoltenVentConfiguration configuration = (MoltenVentConfiguration) pContext.config();
+        MoltenVentConfiguration configuration = pContext.config();
         BlockPos origin = pContext.origin();
         RandomSource random = pContext.random();
-        BlockStateProvider type = pContext.config().getInnerBlock();
-
+        BlockStateProvider typeProvider = pContext.config().getOuterBlock();
+        BlockState type = typeProvider.getState(random, origin);
+        if (type.equals(BlockInit.LOTRUM.get().defaultBlockState())) {
+            if ( !(((Math.cos(Math.toRadians(120)) * origin.getX() - Math.sin(Math.toRadians(120)) * origin.getZ()) < -500) &&
+                    (Math.cos(Math.toRadians(240)) * origin.getX() - Math.sin(Math.toRadians(240)) * origin.getZ()) > 500)) { // Silly goofy math stuff
+                cir.setReturnValue(false);
+            }
+        } else if (type.equals(BlockInit.OSPUM.get().defaultBlockState())) {
+            if ( !(((Math.cos(Math.toRadians(240)) * origin.getX() - Math.sin(Math.toRadians(240)) * origin.getZ()) < -500) &&
+                    (Math.cos(Math.toRadians(360)) * origin.getX() - Math.sin(Math.toRadians(360)) * origin.getZ()) > 500)) {
+                cir.setReturnValue(false);
+            }
+        } else if (type.equals(BlockInit.OCILIUM.get().defaultBlockState())) {
+            if ( !(((Math.cos(Math.toRadians(360)) * origin.getX() - Math.sin(Math.toRadians(360)) * origin.getZ()) < -500) &&
+                    (Math.cos(Math.toRadians(120)) * origin.getX() - Math.sin(Math.toRadians(120)) * origin.getZ()) > 500)) {
+                cir.setReturnValue(false);
+            }
+        }
     }
 }
